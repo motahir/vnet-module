78041a2816069dccee08c1dd30d917c0b651b652
Mo Tahir <mtahir@talk21.com>
2021-01-29 15:23:50 +0000

    .

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "sb" {
   count                = "${length(var.subnet_prefixes)}"
-  name                 = "${element(var.subnet_names,count.index)}"
+  name                 = "${element(var.subnet_names, count.index)}"
   resource_group_name  = "${azurerm_virtual_network.vnet.resource_group_name}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefix       = "${element(var.subnet_prefixes,count.index)}"
+  address_prefix       = "${element(var.subnet_prefixes, count.index)}"
   service_endpoints    = "${var.service_endpoints}"
 }
